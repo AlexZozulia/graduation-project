b2ecc4de96b6ff3938a4c1ff664455c46d79507e
AlexFantasy <57839338+AlexFantasy@users.noreply.github.com>
2020-03-01 13:32:57 +0100

    added open project

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+//Подключаем модули галпа
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const  autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+//Порядок подключения CSS файлов
+const cssFiles = [
+    './src/css/main.css',
+    './src/css/media.css'
+]
+
+//Порядок подключения JS файлов
+const jsFiles = [
+    './src/js/lib.js',
+    './src/js/main.js'
+]
+
+
+//Таск на стили CSS
+function styles() {
+    //Шаблон для поиска файлов CSS
+    //Всей файлы по шаблону './src/css/**/*.css'
+    return gulp.src(cssFiles)
+        //Обьединение файлов в один
+        .pipe(concat('style.css'))
+        //Добавить префиксы
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        //Минификация CSS
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        //Выходная папка для стилей
+        .pipe(gulp.dest('./build/css'))
+        .pipe(browserSync.stream());
+
+}
+
+//Таск на скрипты JS
+function scripts() {
+    //Шаблон для поиска файлов JS
+    //Всей файлы по шаблону './src/js/**/*.js'
+    return gulp.src(jsFiles)
+        //Обьединение файлов в один
+        .pipe(concat('script.js'))
+    //Минификация JS
+        .pipe(uglify())
+        //Выходная папка для скриптов
+        .pipe(gulp.dest('./build/js'))
+        .pipe(browserSync.stream());
+}
+
+//Удалить все в указанной папке
+function clean(){
+    return del(['build/*'])
+}
+
+//Просматривать файлы
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    //Следить за CSS файлами
+    gulp.watch('./src/css/**/*.css', styles)
+    //Следить за JS файлами
+    gulp.watch('./src/js/**/*.js', scripts)
+    //При измененни HTML запустить синхронизацию
+    gulp.watch("./*.html").on('change', browserSync.reload);
+}
+
+//Таск вызывающий функцию styles
+gulp.task('styles', styles);
+//Таск вызывающий функцию scripts
+gulp.task('scripts', scripts);
+//Таск для очистки папки build
+gulp.task('del', clean);
+//Таск для отслкживаня изменений
+gulp.task('watch', watch);
+//Таск для удаления файлов в папке build и запуск styles и scripts
+gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts)));
+//Таск запускает таск build и watch
+gulp.task('dev', gulp.series('build','watch'))
+
